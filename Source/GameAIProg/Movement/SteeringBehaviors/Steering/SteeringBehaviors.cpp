d6df4f5aec6d6658c35084f270a0eddde6d4a2a6
Warre <geertswarre@gmail.com>
2026-02-11 17:42:56 +0100

    Added Debug lines, Seek and Flee

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -1,6 +1,55 @@
 #include "SteeringBehaviors.h"
+
+#include "Dataflow/DataflowEngineUtil.h"
 #include "GameAIProg/Movement/SteeringBehaviors/SteeringAgent.h"
+
+//DEFAULT CLASS
+//*******
+void ISteeringBehavior::DebugLines(ASteeringAgent& Agent, FVector2D& LinearVelocity)
+{
+	FVector Target3D{Target.Position, 0.0f};
+	FVector Agent3D{Agent.GetPosition(), 0.0f};
+
+	constexpr float LineLength = 300.f;
+	const FVector Velocity3D{LinearVelocity, 0.0f};
+	
+	FVector ToEnd = Velocity3D;
+	FVector Dir = ToEnd.GetSafeNormal();
+	FVector FixedEnd = Agent3D + Dir * LineLength;
+	DrawDebugLine(Agent.GetWorld(), Agent3D, FixedEnd, FColor::Green);
+
+	const float AgentDegrees{Agent.GetRotation()};
+	const FRotator AgentRotation{0.f, AgentDegrees, 0.f};
+	const FVector AgentForward{AgentRotation.Vector()};
+	const FVector VelDir{AgentForward};
+	const FVector VelEnd{Agent3D + VelDir * LineLength};
+	DrawDebugLine(Agent.GetWorld(), Agent3D, VelEnd, FColor::Purple);
+
+	DrawDebugCircle(Agent.GetWorld(), Target3D, 20, 10, FColor::Red, false,
+	                -1, 0, 0, FVector(1, 0, 0),
+	                FVector(0, 1, 0));
+}
 
 //SEEK
 //*******
 // TODO: Do the Week01 assignment :^)
+SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
+{
+	SteeringOutput Steering{};
+
+	Steering.LinearVelocity = Target.Position - Agent.GetPosition();
+
+	DebugLines(Agent, Steering.LinearVelocity);
+
+	return Steering;
+}
+
+SteeringOutput Flee::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
+{
+	SteeringOutput Steering{Seek::CalculateSteering(DeltaT, Agent)};
+	Steering.LinearVelocity *= -1;
+
+	DebugLines(Agent, Steering.LinearVelocity);
+
+	return Steering;
+}
--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.h
@@ -3,6 +3,7 @@
 #include <Movement/SteeringBehaviors/SteeringHelpers.h>
 #include "Kismet/KismetMathLibrary.h"
 
+
 class ASteeringAgent;
 
 // SteeringBehavior base, all steering behaviors should derive from this.
@@ -13,16 +14,31 @@ public:
 	virtual ~ISteeringBehavior() = default;
 
 	// Override to implement your own behavior
-	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent & Agent) = 0;
+	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) = 0;
+
+	virtual void DebugLines(ASteeringAgent& Agent, FVector2D& LinearVelocity) final;
 
 	void SetTarget(const FTargetData& NewTarget) { Target = NewTarget; }
-	
-	template<class T, std::enable_if_t<std::is_base_of_v<ISteeringBehavior, T>>* = nullptr>
+
+	template <class T, std::enable_if_t<std::is_base_of_v<ISteeringBehavior, T>>* = nullptr>
 	T* As()
-	{ return static_cast<T*>(this); }
+	{
+		return static_cast<T*>(this);
+	}
 
 protected:
 	FTargetData Target;
 };
 
 // Your own SteeringBehaviors should follow here...
+class Seek : public ISteeringBehavior
+{
+public:
+	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
+};
+
+class Flee : public Seek
+{
+public:
+	virtual SteeringOutput CalculateSteering(float DeltaT, ASteeringAgent& Agent) override;
+};
